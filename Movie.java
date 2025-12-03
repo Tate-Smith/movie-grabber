64e4485db7bce8e917abaf9080535ec421ab6a42
Tate Smith <128739122+Tate-Smith@users.noreply.github.com>
2025-12-03 11:28:59 -0700

    Movie and Main update
    Created a movie class to store info about every movie, and updated main to get data from API and create movie objects out of it

---
+++ Movie.java
@@ -0,0 +1,17 @@
+/*
+ * Purpose: This class is meant to store all the information about the 
+ * movie, like its release date, title, description, and whether ot not
+ * it is an adult film
+ */
+
+public class Movie {
+	private String title;
+	private String date;
+	private String description;
+	private boolean adultMovie;
+	private boolean rating;
+	
+	public Movie(String data) {
+		
+	}
+}
